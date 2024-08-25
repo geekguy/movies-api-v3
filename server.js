f7f2690d46c3193dba5e77057a1120c1d825a35f
Dhaval Trivedi <dhaval6244@gmail.com>
2024-08-25 11:28:30 +0530

    add dotenv

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT;
+console.log({ PORT });
 const movieRouter = require("./routes/movie");
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
